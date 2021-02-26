b6dc58269ded7c6c4bdf5d1774a77ff590fa18d8
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-02-26 16:42:50 +0700

    Native Event Object

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(event){
+      this.name = event.target.value
+    },
     add(num) {
       this.counter = this.counter + num;
     },
